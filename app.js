545cd14a58d474ddfdcfcde1d46ddcdf65592217
daniel <87danielbradley@gmail.com>
2021-11-13 00:53:39 -0500

    Created route to homepage

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+
+const app = express();
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (request, response) => response.send("Home page"))
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
