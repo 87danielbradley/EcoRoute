a6e6a06f62b022243b92eb462cf061bc75fa4a5a
daniel <87danielbradley@gmail.com>
2021-11-14 12:49:40 -0500

    Added redux logger

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -2,11 +2,11 @@ import * as APIutil from "../util/session_api_util";
 import jwt_decode from "jwt-decode"
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECIEVE_USER_LOGOUT = "RECIEVE_USER_LOGOUT";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RESET_SESSION_ERRORS = "RESET_SESSION_ERRORS";
 export const RECIEVE_USER_SIGN_IN = "RECIEVE_USER_SIGN_IN";
-// export const RECIEVE_USER_LOGOUT = "RECIEVE_USER_LOGOUT";
+
 
 const recieveCurrentUser = (user) => ({
     type: RECEIVE_CURRENT_USER,
@@ -17,7 +17,7 @@ const recieveUserSignIn = () => ({
 });
 
 const logoutCurrentUser = () => ({
-    type: LOGOUT_CURRENT_USER
+    type: RECIEVE_USER_LOGOUT
 });
 const receiveSessionErrors = (errors) => ({
     type: RECEIVE_SESSION_ERRORS,
