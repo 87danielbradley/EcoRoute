f027274334c62c32a50d3206d4ee216878dac57b
tkoh13 <tylerkoh93@gmail.com>
2021-11-16 13:02:56 -0500

    add FriendRequest model and additional user routes

--- models/User.js
+++ models/User.js
@@ -13,7 +13,8 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 },{
     timestamps: true
 })
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const FriendRequest = require('../../models/FriendRequest');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
@@ -86,6 +87,86 @@ router.post('/login', (request, response) => {
                 })
         })
 })
+// send friend request
+router.get('/friend_request/:userId', passport.authenticate('jwt', {session: false}), 
+    (request, response) => {
+        const receiver = User.findById(request.params.userId)
+        if (!receiver) {
+            return response.status(404).json({ error: 'User not found' })
+        }
+        if (receiver.friends.includes(request.userId)) {
+            return response.status(400).json({ error: "Y'all already friends" })
+        }
+        if (request.userId == request.params.userId) {
+            return response.status(400).json({ error: 'You cannot send a friend request to yourself' })
+        }
+
+        const friendRequest = FriendRequest.findOne({
+            sender: request.userId,
+            receiver: request.params.userId,
+        })
+
+        if (friendRequest) {
+            return res.status(400).json({ error: 'Friend Request already sent' })
+        }
+
+        const newFriendRequest = new FriendRequest({
+            sender: req.userId,
+            receiver: req.params.userId,
+        })
+
+        newFriendRequest.save()
+        .then(fr => response.json(fr))
+        .catch( err => response.status(400).json({ error: 'Something went wrong' }));
+    }
+);
+// accept friend request
+router.get('/friend_request/:userId/accept', passport.authenticate('jwt', {session: false}),
+    (request, response) => {
+        const friendRequest = FriendRequest.findById(req.params.requestId)
+        if (!friendRequest) {
+            return response.status(404).json({error: 'No request found'})
+        }
+        const sender = User.findById(friendRequest.sender);
+        const reciever = User.findById(friendRequest.reciever);
+        // if (sender.friends.includes(friendRequest.receiver)) {
+        //     return response.status(400).json({error: 'Already friends'})
+        // }
+        sender.friends.push(reciever.userId)
+        sender.save()
+        .then(fr => response.json(fr))
+        .catch( err => response.status(400).json({ error: 'Something went wrong' }));
+        
+        reciever.friends.push(sender.userId)
+        reciever.save()
+        .then(fr => response.json(fr))
+        .catch( err => response.status(400).json({ error: 'Something went wrong' }));
+    }
+);
+// cancel friend request 
+router.get('/friend_request/:userId/cancel', passport.authenticate('jwt', {session: false}),
+    (request, response) => {
+        const friendRequest = FriendRequest.findById(req.params.requestId)
+        if (!friendsRequest) {
+            return response.status(404).json({ error: 'Request already cenceled or not sended yet' })
+        }
+        FriendRequest.deleteOne({ _id: req.params.requestId })
+        .then(() => response.status(200).json({ message: 'Friend request canceled' }))
+        .catch( err => response.status(400).json({ error: 'Something went wrong' }));
+    }
+);
+// decline friend request 
+router.get('/friend_request/:userId/decline', passport.authenticate('jwt', {session: false}),
+    (request, response) => {
+        const friendRequest = FriendRequest.findById(req.params.requestId)
+        if (!friendRequest) {
+            return response.status(404).json({ error: 'Request already declined or not sended yet' })
+        }
+        FriendRequest.deleteOne({ _id: req.params.requestId })
+        .then(() => response.status(200).json({ message: 'Friend request declined' }))
+        .catch( err => response.status(400).json({ error: 'Something went wrong' }));
+    }
+);
 
 //private auth route
 router.get('/current', passport.authenticate('jwt', {session: false}), (request, response) => {
