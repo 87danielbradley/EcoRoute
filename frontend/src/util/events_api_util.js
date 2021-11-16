3ad26a7f52b1a08c67ad2e47e4545750fd5d3836
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-16 11:28:30 -0500

    api util updated

--- frontend/src/util/events_api_util.js
+++ frontend/src/util/events_api_util.js
@@ -4,16 +4,19 @@ import axios from "axios";
 //     return axios.get('api/events')
 // };
 export const getUserEvents = userId => {
-    return axios.get(`api/events/user/${userId}`)
+    return axios.get(`api/user/${userId}`)
 };
 
+export const getEvent = eventId => {
+    return axios.get(`api/events/${eventId}`)
+}
 export const createEvent = event => {
-    return axios.post('api/events', event)
+    return axios.post('api/events/', event)
 }
 
-export const updateEvent = event => {
-    return axios.patch('api/events', event)
-}
+// export const updateEvent = event => {
+//     return axios.patch('api/events', event)
+// }
 
 export const deleteEvent = eventId => {
     return axios.delete(`api/events/${eventId}`)
