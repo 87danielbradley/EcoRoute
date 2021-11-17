f9824127bc91fe7f2b18e3529e42a81bfc41fd48
sparsons808 <83877102+sparsons808@users.noreply.github.com>
2021-11-17 18:20:20 -0500

    create friend actions

--- frontend/src/actions/friend_actions.js
+++ frontend/src/actions/friend_actions.js
@@ -3,5 +3,18 @@ import * as APIUtil from "../util/friends_api_util";
 export const RECEIVE_FRIEND = "RECEIVE_FRIEND";
 export const REMOVE_FRIEND_REQUEST = "REMOVE_FRIEND_REQUEST";
 
+const receiveFriend = friend => ({
+    type: RECEIVE_FRIEND,
+    friend
+});
+
+export const fetchFriend = userId => dispatch => {
+    return (
+        APIUtil.getFriend(userId)
+            .then( friend => dispatch(receiveFriend(friend)))
+            .catch(err => console.log(err))
+    )
+};
+
 
 
