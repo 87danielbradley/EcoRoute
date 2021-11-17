c739fee5db38d45b7c026eddd3286896ab3c3817
daniel <87danielbradley@gmail.com>
2021-11-17 10:53:33 -0500

    Merged mickeys branch into mapbox

--- frontend/src/reducers/events_reducer.js
+++ frontend/src/reducers/events_reducer.js
@@ -1,30 +1,58 @@
 import { RECEIVE_USER_EVENTS, RECEIVE_EVENT, REMOVE_EVENT } from "../actions/event_actions";
 
-// const initialState = {
-//    1: {
-//     title: 'Beethoven Concert',
-//     category: 'Music',
-//     user: 100,
-//     attendees: ["mickey", "daniel"],
-//     id: 1,
-//     date: 'November 24, 2021',
+const initialState = {
+   1: {
+    title: 'Beethoven Concert',
+    category: 'Music',
+    user: 100,
+    attendees: [
+        {geometry: {coordinates: [-73.906021, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906022, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906023, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906024, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906025, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906026, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906027, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906028, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906029, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906030, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906031, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906032, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906033, 40.745541], type: "Point"}}
+    ],
+    id: 1,
+    date: 'November 24, 2021',
 
-//   },
+  },
   
-//   2: {
-//    title: 'Dinner Pary',
-//     category: 'Food',
-//       user: 101,
-//     attendees: ["sydney", "tyler"],
-//     id: 2,
-//     date: 'November 24, 2021',
+  2: {
+   title: 'Dinner Pary',
+    category: 'Food',
+      user: 101,
+    attendees: [
+        {geometry: {coordinates: [-73.906021, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906022, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906023, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906024, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906025, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906026, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906027, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906028, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906029, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906030, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906031, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906032, 40.745541], type: "Point"}},
+        {geometry: {coordinates: [-73.906033, 40.745541], type: "Point"}}
+    ],
+    id: 2,
+    date: 'November 24, 2021',
   
-//     }
+    }
 
-// }
+}
 
 
-const EventsReducer = (oldState = {}, action) => {
+const EventsReducer = (oldState = initialState, action) => {
     Object.freeze(oldState);
     let nextState = Object.assign({}, oldState);
 
