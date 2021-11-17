955cb8910e39570c8e1153505938312e8e8768ed
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-17 11:57:29 -0500

    sign up error

---
+++ config/key_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://mickeysaddai:0lk2fzON2aypk6gC@cluster0.tjxi3.mongodb.net/ecoroute?retryWrites=true&w=majority",
+    secretOrKey: 's1e2c3r4e5t6k7e8y9' //Make sure this is your own unique string
+}
