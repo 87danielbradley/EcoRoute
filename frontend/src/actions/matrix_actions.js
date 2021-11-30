e137c4eae6098d33be736111c604427103c2a6a3
daniel <87danielbradley@gmail.com>
2021-11-29 22:03:15 -0500

    Updated mapbox matrix actions

--- frontend/src/actions/matrix_actions.js
+++ frontend/src/actions/matrix_actions.js
@@ -2,6 +2,7 @@ import * as APIutil from "../util/messages_api_util";
 import jwt_decode from "jwt-decode"
 
 export const RECEIVE_MATRIX = "RECEIVE_MATRIX";
+export const RECEIVE_PLACES = "RECEIVE_PLACES";
 
 
 
@@ -9,9 +10,16 @@ const receiveMatrix = (matrix) => ({
     type: RECEIVE_MATRIX,
     matrix
 });
+const receiveNearbyPlaces = (places) => ({
+    type: RECEIVE_PLACES,
+    places
+});
 
 
-export const fetchMatrix = () => dispatch => APIutil.getMatrix()
+export const fetchMatrix = (attendeesArray, placesArray) => dispatch => APIutil.getMatrix(attendeesArray, placesArray)
     .then( (matrix) => dispatch(receiveMatrix(matrix)));
+
+export const findPlacesNearby = (query, nearby) => dispatch => APIutil.getPlaces(query, nearby)
+    .then( (places) => dispatch(receiveNearbyPlaces(places)));
     
 
--- frontend/src/util/matrix_api_util.js
+++ frontend/src/util/matrix_api_util.js
@@ -11,5 +11,9 @@ export const getMatrix = (attendees, places) => {
 // location example
 // -122.42,37.78;-122.45,37.91;-122.48,37.73
 
+export const getPlaces = (query, nearby) => {
+    return axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=${nearby.join(',')}&access_token=${accessToken}`)
+};
 
-
+//output example
+//https://api.mapbox.com/geocoding/v5/mapbox.places/peets.json?proximity=-122.3995752,37.7881856&access_token=pk.eyJ1IjoiODdkYW5pZWxicmFkbGV5IiwiYSI6ImNrdzExeXNpczd4NXkzMXMxc3BsZmlpaWkifQ.PQzQgyjHRXQ2ZRyGMdxl7g
