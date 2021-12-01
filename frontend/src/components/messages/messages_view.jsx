3e30000547bf0a6bb080aabb37fd743dff428ff4
tkoh13 <tylerkoh93@gmail.com>
2021-12-01 11:05:55 -0500

    debug new messages

--- frontend/src/components/messages/messages_view.jsx
+++ frontend/src/components/messages/messages_view.jsx
@@ -22,14 +22,15 @@ class MessagesView extends React.Component{
     }
     componentDidMount(){
         // this.props.fetchMessages()
-        console.log(`componentDidMount`)
-        console.log(this.state)
-        console.log(this.props)
+        console.log("mounted state", this.state)
+        console.log("mounted props", this.props)
         this.props.fetchEventMessages(this.props.eventId)
             .then(response => {
+                console.log("mounted response", response)
             // .then(response => console.log(response));
                 
-                return(this.setState({messages: response.messages.data}))
+                // return(this.setState({eventId: this.props.eventId, messages: this.props.messages}))
+                return(this.setState({eventId: this.props.eventId, messages: response.messages.data}))
                 });
     }
     // componentWillReceiveProps(newState) {
@@ -38,7 +39,6 @@ class MessagesView extends React.Component{
 
 
     update() {
-        
         return (event) => this.setState({
             text: event.currentTarget.value
         })
@@ -48,15 +48,19 @@ class MessagesView extends React.Component{
         event.preventDefault();
         this.props.createMessage({text: this.state.text,
         eventId: this.state.eventId});
-        this.setState({text: this.state.text})
+        this.setState({text: ""})
+        // this.props.fetchEventMessages(this.state.eventId)
+        //     .then(response => {
+        //         return (this.setState({ eventId: this.state.eventId, messages: response.messages.data }))
+        //     })
+        // this.setState({text: this.state.text})
     }
 
     componentDidUpdate(prevProps){
-        console.log(`componentDidUpdate`)
-        console.log("state", this.state)
-        console.log("messages", this.state.messages)
-        console.log("eventId", this.props.eventId)
-        if (this.props.eventId !== prevProps.eventId){
+        console.log("updated state", this.state)
+        console.log("updated state.messages", this.state.messages)
+        console.log("updated props.eventId", this.props.eventId)
+        if (this.props.eventId !== prevProps.eventId) { //|| this.props.messages !== prevProps.messages){
             // debugger
             // this.setState({eventId: this.props.eventId,
             // messages: this.props.messages})
@@ -66,17 +70,27 @@ class MessagesView extends React.Component{
 
                     return (this.setState({ eventId: this.props.eventId, messages: response.messages.data }))
                 });
+        } else if (this.props.newMessage !== prevProps.newMessage) {
+            console.log("this.props.messages", this.props.messages)
+            console.log("prevProps.messages", prevProps.messages)
+            // return (this.setState({ messages: this.props.messages }))
+
+            this.props.fetchEventMessages(this.props.eventId)
+                .then(response => {
+                    return (this.setState({ eventId: this.props.eventId, messages: response.messages.data }))
+                });
         }
     }
     render(){
-        
-        if (!this.state.messages) {
+        // if (!this.state.messages) {
+        if (this.state.messages === null || this.state.messages.length === 0) {
         // if (this.state.messages.length === 0) {
             return (
             <div>
                 <form onSubmit={this.handleSubmit} >
                     <input id="temp-input" 
                         type="text"
+                        placeholder="Send a message"
                         onChange={this.update()}>
                     </input>
                     <input type="submit" value="&#9998;"></input>
@@ -89,6 +103,7 @@ class MessagesView extends React.Component{
                 <form onSubmit={this.handleSubmit} >
                     <input id="temp-input" 
                         type="text"
+                        placeholder="Send a message"
                         onChange={this.update()}>
                     </input>
                     <input type="submit" value="&#9998;"></input>
