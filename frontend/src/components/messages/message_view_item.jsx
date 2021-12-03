20a860fd4107fe8c5d4ee19ee0e9efc4675ae82a
daniel <87danielbradley@gmail.com>
2021-12-03 17:43:56 -0500

    css update

--- frontend/src/components/messages/message_view_item.jsx
+++ frontend/src/components/messages/message_view_item.jsx
@@ -8,7 +8,7 @@ class MessagesViewItem extends React.Component{
     render(){
         const ownMessage = (this.props.currentUser.id === this.props.message.sender ? 'message-item-left' : 'message-item-right')
         // const ownMessage = (this.props.currentUser.id === this.props.message.sender ? 'true' : 'false')
-        debugger
+        
         
         return(
             <div className='message-container'>
